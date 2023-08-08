ca95d4d67d82999301ad4b383c0cc63ecee9dbea
Milgevskaya Anastasia <amilgvs@gmail.com>
2023-08-08 23:16:13 +0500

    ex002

---
+++ Example002/Program.cs
@@ -0,0 +1,5 @@
+﻿// ввод имени с консоли, вывод Привет, Имя
+Console.WriteLine ("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.writeLine(username);
